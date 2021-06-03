c5859c82f3e669775958096cbf11c4125404091f
bphogat1006 <b.phogat1925@gmail.com>
2021-06-03 18:19:39 -0400

    Promisified all XHRs

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 
-var redirect_uri = "http://127.0.0.1:5500/index.html"
+/*
+var redirect_uri = ""
+var client_id = ""
+var client_secret = ""
+*/
 
 var access_token = null
 var refresh_token = null
 
 var fetchPeriod = "short_term"
+var refreshingAccessToken = false
 
 const AUTHORIZE = "https://accounts.spotify.com/authorize"
 const TOKEN = "https://accounts.spotify.com/api/token"
@@ -24,9 +29,33 @@ function onPageLoad() {
     // otherwise use access token and API to get user data
     else {
       document.getElementById("user-data").style.display = 'block'
+
+      // see if access token is expired
       testAccessToken()
-      getUserProfile()
-      getTopListens()
+      .catch((status) => {
+        if(status==401) {return refreshAccessToken()}
+      })
+      // if so, refresh it
+      // then get user profile info
+      .then((data) => {
+        if(data != undefined && JSON.parse(data).access_token != undefined) {
+          handleAuthorizationResponse(data)
+        }
+        return getUserProfile()
+      })
+      // then get top artists
+      .then((data) => {
+        handleUserProfileResponse(data)
+        return getTopListens("artists")
+      })
+      // then get top tracks
+      .then((data) => {
+        handleTopListensResponse(data)
+        return getTopListens("tracks")
+      })
+      .then((data) => {
+        handleTopListensResponse(data)
+      })
     }
   }
 }
@@ -38,7 +67,7 @@ function requestAuthorization() {
   url += "&response_type=code"
   url += "&redirect_uri=" + encodeURI(redirect_uri)
   url += "&show_dialog=true"
-  url += "&scope=user-read-private user-read-email user-modify-playback-state user-read-playback-position user-library-read streaming user-read-playback-state user-read-recently-played playlist-read-private user-top-read"
+  url += "&scope=user-top-read user-library-read playlist-read-private playlist-read-collaborative"
   window.location.href = url // Show Spotify's authorization screen
 }
 
@@ -62,157 +91,168 @@ function fetchAccessToken(code) {
   body += "&redirect_uri=" + encodeURI(redirect_uri)
   body += "&client_id=" + client_id
   body += "&client_secret=" + client_secret
-  callAuthorizationApi(body)
+  makeAuthorizationRequest(body)
+  .then((data) => {
+    handleAuthorizationResponse(data)
+  })
 }
 
 // use refresh token to fetch new access token
 function refreshAccessToken() {
+  refreshingAccessToken = true
   refresh_token = localStorage.getItem("refresh_token")
   var body = "grant_type=refresh_token"
   body += "&refresh_token=" + refresh_token
   body += "&client_id=" + client_id
-  callAuthorizationApi(body)
+  return makeAuthorizationRequest(body)
 }
 
 // request access & refresh tokens
-function callAuthorizationApi(body) {
-  var xhr = new XMLHttpRequest()
-  xhr.open("POST", TOKEN)
-  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
-  xhr.setRequestHeader('Authorization', 'Basic ' + btoa(client_id + ":" + client_secret))
-  xhr.send(body)
-  xhr.onload = handleAuthorizationResponse
+function makeAuthorizationRequest(body) {
+  return new Promise(function (resolve, reject) {
+    var xhr = new XMLHttpRequest()
+    xhr.open("POST", TOKEN)
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
+    xhr.setRequestHeader('Authorization', 'Basic ' + btoa(client_id + ":" + client_secret))
+    xhr.send(body)
+    xhr.onload = function () {
+      if (this.status == 200) {
+        console.log(JSON.parse(this.responseText))
+        resolve(this.responseText);
+      } else {
+        var error = this.status + ": " + this.responseText
+        console.log(error)
+        alert(error)
+        reject(this.status, this.responseText)
+      }
+    };
+    xhr.onerror = function () {
+      var error = this.status + ": " + this.responseText
+      console.log(error)
+      alert(error)
+      reject(this.status, this.responseText)
+    };
+  });
 }
 
 // store access & refresh tokens
-function handleAuthorizationResponse() {
-  if(this.status == 200) {
-    var data = JSON.parse(this.responseText)
-    console.log(data)
-    var data = JSON.parse(this.responseText)
-    if(data.access_token != undefined) {
-      access_token = data.access_token
-      localStorage.setItem("access_token", access_token)
-      // alert('new access token')
-    }
-    if(data.refresh_token != undefined) {
-      refresh_token = data.refresh_token
-      localStorage.setItem("refresh_token", refresh_token)
-      // alert('new refresh token')
-    }
+function handleAuthorizationResponse(responseText) {
+  var data = JSON.parse(responseText)
+  if(data.access_token != undefined) {
+    access_token = data.access_token
+    localStorage.setItem("access_token", access_token)
+    // alert('new access token')
+  }
+  if(data.refresh_token != undefined) {
+    refresh_token = data.refresh_token
+    localStorage.setItem("refresh_token", refresh_token)
+    // alert('new refresh token')
+  }
+  if(!refreshingAccessToken) {
     onPageLoad()
-  }
-  else {
-    console.log(this.responseText)
-    alert(this.responseText)
+  } else {
+    refreshingAccessToken = false
   }
 }
 
-// generic call to the Spotify API using access token
-function callApi(method, endpoint, body, callback) {
-  var xhr = new XMLHttpRequest();
-  xhr.open(method, endpoint)
-  xhr.setRequestHeader('Content-Type', 'application/json')
-  xhr.setRequestHeader('Authorization', 'Bearer ' + access_token)
-  xhr.send(body)
-  xhr.onload = callback
+// Promisified XMLHttpRequest to the Spotify API using access token
+function makeAPIRequest (method, endpoint, body) {
+  return new Promise(function (resolve, reject) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, endpoint)
+    xhr.setRequestHeader('Content-Type', 'application/json')
+    xhr.setRequestHeader('Authorization', 'Bearer ' + access_token)
+    xhr.send(body)
+    xhr.onload = function () {
+      if (this.status == 200) {
+        console.log(JSON.parse(this.responseText))
+        resolve(this.responseText);
+      } else {
+        if(this.status == 401) {
+          // alert("access token expired")
+        } else {
+          var error = this.status + ": " + this.responseText
+          console.log(error)
+          alert(error)
+        }
+        reject(this.status, this.responseText)
+      }
+    };
+    xhr.onerror = function () {
+      if(this.status == 401) {
+        // alert("access token expired")
+      } else {
+        var error = this.status + ": " + this.responseText
+        console.log(error)
+        alert(error)
+      }
+      reject(this.status, this.responseText)
+    };
+  });
 }
 
-/** API calls the website will make to gather data */
+/** API calls the website will make to gather user data */
 
 function testAccessToken() {
-  callApi("GET", "https://api.spotify.com/v1/me", null, handleTestAccessTokenResponse)
-}
-
-function handleTestAccessTokenResponse() {
-  if(this.status == 401) {
-    refreshAccessToken()
-  }
+  return makeAPIRequest("GET", "https://api.spotify.com/v1/me", null)
 }
 
 function getUserProfile() {
-  callApi("GET", "https://api.spotify.com/v1/me", null, handleUserProfileResponse)
+  return makeAPIRequest("GET", "https://api.spotify.com/v1/me", null)
 }
 
-function handleUserProfileResponse() {
-  if(this.status == 200) {
-    var data = JSON.parse(this.responseText)
-    console.log(data)
-
-    document.getElementById("display-name").innerHTML += '<small class="text-muted">' + data.display_name + '</small>'
-    document.getElementById("profile-picture").setAttribute("src", data.images[0].url)
-  }
-  else if(this.status == 401) {
-    alert("access token expired")
-    refreshAccessToken()
-  }
-  else {
-    console.log(this.responseText)
-    alert(this.responseText)
-  }
+function handleUserProfileResponse(responseText) {
+  var data = JSON.parse(responseText)
+  document.getElementById("display-name").innerHTML += '<small class="text-muted">' + data.display_name + '</small>'
+  document.getElementById("profile-picture").setAttribute("src", data.images[0].url)
 }
 
 function getTopListens(type) {
-  if(typeof type != "string") {type="artists"}
   document.getElementById("loading-top-listens").style.display = "block"
   var queryParams = "?time_range=" + fetchPeriod
   queryParams += "&limit=50"
   queryParams += "&offset=0"
-  callApi("GET", "https://api.spotify.com/v1/me/top/"+type+queryParams, null, handleTopListensResponse)
+  return makeAPIRequest("GET", "https://api.spotify.com/v1/me/top/"+type+queryParams, null, handleTopListensResponse)
 }
 
-function handleTopListensResponse() {
-  if(this.status == 200) {
-    var data = JSON.parse(this.responseText)
-    console.log(data)
+function handleTopListensResponse(responseText) {
+  var data = JSON.parse(responseText)
+  var type = (data.items[0].album) ? "tracks" : "artists"
+  var topListensElement = document.getElementById("top-"+type+"-list")
 
-    var type = (data.items[0].album) ? "tracks" : "artists"
-    var topListensElement = document.getElementById("top-"+type+"-list")
+  data.items.forEach(item => {
+    var li = document.createElement("li")
+    li.setAttribute("class", "list-group-item d-flex align-items-center ps-4")
 
-    data.items.forEach(item => {
-      var li = document.createElement("li")
-      li.setAttribute("class", "list-group-item d-flex align-items-center ps-4")
+    var thumbnail = document.createElement("img")
+    thumbnail.setAttribute("class", "me-3")
+    thumbnail.setAttribute("width", "50em")
+    thumbnail.setAttribute("height", "50em")
 
-      var thumbnail = document.createElement("img")
-      thumbnail.setAttribute("class", "me-3")
-      thumbnail.setAttribute("width", "50em")
-      thumbnail.setAttribute("height", "50em")
-
-      if(type == "artists") {
-        thumbnail.setAttribute("src", item.images[0].url)
-        li.appendChild(thumbnail)
-        var artist = document.createElement("div")
-        artist.innerHTML = item.name
-        li.appendChild(artist)
-      }
-      else {
-        thumbnail.setAttribute("src", item.album.images[0].url)
-        li.appendChild(thumbnail)
-        var songInfo = document.createElement("div")
-        songInfo.setAttribute("class", "")
-        var name = document.createElement("div")
-        var artist = document.createElement("div")
-        artist.setAttribute("class", "text-muted")
-        name.innerHTML = item.name
-        artist.innerHTML = item.artists[0].name
-        songInfo.appendChild(name)
-        songInfo.appendChild(artist)
-        li.appendChild(songInfo)
-      }
-      topListensElement.appendChild(li)
-    });
     if(type == "artists") {
-      getTopListens("tracks")
-    } else {
-      document.getElementById("loading-top-listens").style.display = "none"
+      thumbnail.setAttribute("src", item.images[0].url)
+      li.appendChild(thumbnail)
+      var artist = document.createElement("div")
+      artist.innerHTML = item.name
+      li.appendChild(artist)
     }
-  }
-  else if(this.status == 401) {
-    alert("access token expired")
-    refreshAccessToken()
-  }
-  else {
-    console.log(this.responseText)
-    alert("top listens error: " + this.responseText)
+    else {
+      thumbnail.setAttribute("src", item.album.images[0].url)
+      li.appendChild(thumbnail)
+      var songInfo = document.createElement("div")
+      songInfo.setAttribute("class", "")
+      var name = document.createElement("div")
+      var artist = document.createElement("div")
+      artist.setAttribute("class", "text-muted")
+      name.innerHTML = item.name
+      artist.innerHTML = item.artists[0].name
+      songInfo.appendChild(name)
+      songInfo.appendChild(artist)
+      li.appendChild(songInfo)
+    }
+    topListensElement.appendChild(li)
+  });
+  if(type == "tracks") {
+    document.getElementById("loading-top-listens").style.display = "none"
   }
 }
