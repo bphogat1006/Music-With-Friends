a7550d18cb63c8d1f0156aa8dfeffa7b403d91fc
Bhavya Phogat <b.phogat1925@gmail.com>
2021-06-13 19:34:53 -0400

    Update credentials.js

--- credentials.js
+++ credentials.js
@@ -1,5 +1,5 @@
 
-var redirect_uri = "http://127.0.0.1:5500/index.html"
+var redirect_uri = ""
 
-var client_id = "f7e601865cae4caa99d3e8aa643825fb"
-var client_secret = "b7fa8990184142c18200913172e660b1"
+var client_id = ""
+var client_secret = ""
