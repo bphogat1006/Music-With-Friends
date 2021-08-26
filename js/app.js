c838c4554cec4ca189058bef59ba3ea0d451869a
bphogat1006 <b.phogat1925@gmail.com>
2021-08-26 16:21:21 -0400

    remove unnecessary user scopes

--- js/app.js
+++ js/app.js
@@ -71,7 +71,7 @@ function requestAuthorization() {
   url += "&response_type=code"
   url += "&redirect_uri=" + encodeURI(redirect_uri)
   url += "&show_dialog=true"
-  url += "&scope=user-top-read user-library-read playlist-read-private playlist-read-collaborative playlist-modify-public playlist-modify-private"
+  url += "&scope=user-top-read user-library-read playlist-read-private playlist-read-collaborative playlist-modify-public"
   window.location.href = url // Show Spotify's authorization screen
 }
 
